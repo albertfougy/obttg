17404ed6077cddbdc4831769fdc544e92d82d927
Albert Fougy <albert@fougy.com>
2018-08-26 18:31:53 -0400

    switched to psql

--- functional_tests.py
+++ functional_tests.py
@@ -41,7 +41,7 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
+		time.sleep(10)
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very
@@ -50,9 +50,11 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
+
 		# The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
