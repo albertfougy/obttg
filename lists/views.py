2c4fdcb01b82d96b5aa9e366bac0906b17b82910
Albert Fougy <albert@fougy.com>
2018-08-26 22:04:13 -0400

    finished section Saving the Post to DB Chapter 5

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+	if request.method == 'POST':
+		new_item_text = request.POST['item_text']
+		Item.objects.create(text = new_item_text)
+	else:
+		new_item_text = ''
+
 	return render(request, 'home.html', {
-		'new_item_text': request.POST.get('item_text', ''),
+		'new_item_text' : new_item_text,
 	})
