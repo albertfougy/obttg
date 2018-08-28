560b68a6300f19418cd9c7c9af330fa192c41a64
Albert Fougy <albert@fougy.com>
2018-08-28 08:05:07 -0400

    adjusted settings, now postrgres and django ports do not clash anymore

--- superlists/settings.py
+++ superlists/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME',''),
         'USER': os.environ.get('DB_USER',''),
         'PASSWORD': os.environ.get('DB_PASS',''),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
