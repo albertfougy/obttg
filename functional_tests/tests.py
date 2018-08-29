b57375ef44a974870ad2c1efd7552f5d8b5e16a8
Albert Fougy <albert@fougy.com>
2018-08-29 18:40:15 -0400

    Finished section: A New View Function

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -66,44 +66,45 @@ class NewVisitorTest(LiveServerTestCase):
 		# Satisfied, she goes back to sleep
 
 	def test_multiple_users_can_start_lists_at_different_urls(self):
-		# Edith starts a new to-do lists
-		self.browser.get(self.live_server_url)
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy peacock	feathers')
-		inputbox.send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+	    # Edith starts a new to-do list
+	    self.browser.get(self.live_server_url)
+	    inputbox = self.browser.find_element_by_id('id_new_item')
+	    inputbox.send_keys('Buy peacock feathers')
+	    inputbox.send_keys(Keys.ENTER)
+	    self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-		#Seh notices that her list has a unique live_server_url
-		edith_list_url = self.browser.current_url
-		self.assertRegex(edith_list_url, '/lists/.+')
-		## Now a new user, Francis, comes along to the site.
+	    # She notices that her list has a unique URL
+	    edith_list_url = self.browser.current_url
+	    self.assertRegex(edith_list_url, '/lists/.+')
+	    # Now a new user, Francis, comes along to the site.
 
-		## We use a new browser session to make sure that no information
-		## of Edith's is coming through from cookies etc
+	    ## We use a new browser session to make sure that no information
+	    ## of Edith's is coming through from cookies etc
+	    self.browser.quit()
+	    self.browser = webdriver.Firefox()
 
-		self.browser.quit()
-		self.browswer = webdriver.Firefox()
+	    # Francis visits the home page.  There is no sign of Edith's
+	    # list
+	    self.browser.get(self.live_server_url)
+	    page_text = self.browser.find_element_by_tag_name('body').text
+	    self.assertNotIn('Buy peacock feathers', page_text)
+	    self.assertNotIn('make a fly', page_text)
 
-		# Francis visits the home page. There is no sign of Edith's list
-		self. browser. get(self.live_server_url)
-		page_text = self.browser.find_element_by_tag_name('body').text
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertNotIn('make a fly', page_text)
+	    # Francis starts a new list by entering a new item. He
+	    # is less interesting than Edith...
+	    inputbox = self.browser.find_element_by_id('id_new_item')
+	    inputbox.send_keys('Buy milk')
+	    inputbox.send_keys(Keys.ENTER)
+	    self.wait_for_row_in_list_table('1: Buy milk')
 
-		# Francis starts a new list by entering a new item.
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy milk')
-		inputbox.send_keys(Key.ENTER)
-		self.wait_for_row_in_list_table('1: Buy milk')
+	    # Francis gets his own unique URL
+	    francis_list_url = self.browser.current_url
+	    self.assertRegex(francis_list_url, '/lists/.+')
+	    self.assertNotEqual(francis_list_url, edith_list_url)
 
-		# Francis gets his own URL
-		francis_list_url = self.browser.current_url
-		self.assertRegex(francis_list_url, '/lists/.+')
-		self.assertNotEqual(francis_list_url, edith_list_url)
-
-		# Again, there is no trace of Edith's list
-		page_text = self.browser.find_element_by_tag_name('body').text
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertIn('Buy milk', page_text)
+	    # Again, there is no trace of Edith's list
+	    page_text = self.browser.find_element_by_tag_name('body').text
+	    self.assertNotIn('Buy peacock feathers', page_text)
+	    self.assertIn('Buy milk', page_text)
 
 		# Satisfied, they both go back to sleep
