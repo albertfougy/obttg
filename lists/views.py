739d490811baa2f352a0d65291e925d548844207
Albert Fougy <albert@fougy.com>
2018-08-29 18:40:15 -0400

    Finished section: A New View Function

--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,11 @@ from lists.models import Item
 def home_page(request):
 	if request.method == 'POST':
 		Item.objects.create(text = request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world')
+		return redirect('/lists/the-only-list-in-the-world/')
 
+	items = Item.objects.all()
+	return render(request, 'home.html', {'items': items})
+
+def view_list(request):
 	items = Item.objects.all()
 	return render(request, 'home.html', {'items': items})
