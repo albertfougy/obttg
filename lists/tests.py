0e64cac4a078eea1796e001eee1ebc60cd979e85
Albert Fougy <albert@fougy.com>
2018-08-29 19:30:40 -0400

    "new URL, view and template to display lists"

--- lists/tests.py
+++ lists/tests.py
@@ -8,10 +8,6 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-	def test_home_page_returns_correct_html(self):
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
-
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
@@ -31,15 +27,6 @@ class HomePageTest(TestCase):
 	def test_only_saves_items_when_necessary(self):
 		self.client.get('/')
 		self.assertEqual(Item.objects.count(), 0)
-
-	def test_displays_all_list_items(self):
-		Item.objects.create(text= 'itemey 1')
-		Item.objects.create(text = 'itemey 2')
-
-		response = self.client.get('/')
-
-		self.assertIn('itemey 1', response.content.decode())
-		self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -62,6 +49,10 @@ class ItemModelTest(TestCase):
 		self.assertEqual(second_saved_item.text, 'Item the second')
 
 class ListViewTest(TestCase):
+
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
 		Item.objects.create(text= 'itemey 1')
