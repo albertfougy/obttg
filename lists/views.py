38d25fb487cfac0bf9a77dec171c0d1071863f37
Albert Fougy <albert@fougy.com>
2018-08-30 05:28:02 -0400

    Finished section: A Foreign Key Relationship

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -13,5 +13,6 @@ def view_list(request):
 	return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-	Item.objects.create(text = request.POST['item_text'])
+	list_ = List.objects.create()
+	Item.objects.create(text = request.POST['item_text'], list = list_)
 	return redirect('/lists/the-only-list-in-the-world/')
