0eda9373e6a1762a9066e53941515cd5312faa36
Albert Fougy <albert@fougy.com>
2018-08-30 14:07:21 -0400

    major update, will have to re-visit due to some glitches in code

--- lists/views.py
+++ lists/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import redirect, render
 from lists.models import Item, List
 
-# Create your views here.
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text = request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-	return render(request, 'home.html')
+    return render(request, 'home.html')
 
-def view_list(request):
-	items = Item.objects.all()
-	return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-	list_ = List.objects.create()
-	Item.objects.create(text = request.POST['item_text'], list = list_)
-	return redirect('/lists/the-only-list-in-the-world/')
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
