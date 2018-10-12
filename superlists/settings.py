0b619eef9c0d4ee1afc095290461399f108dc96a
Albert Fougy <albert@fougy.com>
2018-10-11 21:12:51 -0400

    psycops2 deprecated

--- superlists/settings.py
+++ superlists/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME',''),
         'USER': os.environ.get('DB_USER',''),
         'PASSWORD': os.environ.get('DB_PASS',''),
