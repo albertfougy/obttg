d19c4174447d37f24c08b07f031e2c6def1ebcbd
Albert Fougy <albert@fougy.com>
2018-10-11 21:36:07 -0400

    psycops2 try it again

--- superlists/settings.py
+++ superlists/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME',''),
         'USER': os.environ.get('DB_USER',''),
         'PASSWORD': os.environ.get('DB_PASS',''),
