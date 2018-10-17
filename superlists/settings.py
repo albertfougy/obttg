a03e7f85e5d45f132ac3b1d6b4ed650e69914b57
Albert Fougy <albert@fougy.com>
2018-10-17 01:14:29 -0400

    fixed typo in settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = os.environ['SITENAME']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
