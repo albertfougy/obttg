12658888acb84327f243bb0edbc0347e92293615
Ubuntu <ubuntu@ip-172-31-18-126.ec2.internal>
2018-10-25 00:47:29 +0000

    fixed bug related to endpoints malformed

--- superlists/urls.py
+++ superlists/urls.py
@@ -9,15 +9,16 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import include, url
 from lists import views as list_views
-from lists import urls as list_urls
+
 
 urlpatterns = [
-    url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/', include(list_urls)),
+    # url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/new$', list_views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', list_views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', list_views.add_item, name='add_item'),
+    # url(r'^test_view$', list_views.test_view, name='test_view')
 ]
