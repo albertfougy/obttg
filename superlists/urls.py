b647fc271a15347f96877a12d3724ed5fe00b5ce
Albert Fougy <albert@fougy.com>
2018-10-27 10:16:54 -0400

    commented out the extra endpoints

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,12 +13,18 @@ Class-based views
 
 from django.conf.urls import include, url
 from lists import views as list_views
+from lists import urls as list_urls
 
+
+# urlpatterns = [
+#     url(r'^$', list_views.home_page, name='home'),
+#     url(r'^lists/new$', list_views.new_list, name='new_list'),
+#     url(r'^lists/(\d+)/$', list_views.view_list, name='view_list'),
+#     url(r'^lists/(\d+)/add_item$', list_views.add_item, name='add_item'),
+#     url(r'^test_view$', list_views.test_view, name='test_view')
+# ]
 
 urlpatterns = [
-    # url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/new$', list_views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', list_views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', list_views.add_item, name='add_item'),
-    # url(r'^test_view$', list_views.test_view, name='test_view')
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
 ]
