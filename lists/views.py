a9eb68585a5417ca449e2deb5caa1d6a16988749
Albert Fougy <albert@fougy.com>
2018-12-30 13:46:32 -0500

    replaced hardcoded URLs in html files. Looking to solve DB problem before finishing chapter

--- lists/views.py
+++ lists/views.py
@@ -21,7 +21,8 @@ def new_list(request):
       list_.delete()
       error = "You can't have an empty list item"
       return render(request, 'home.html', {"error" : error})
-    return redirect(f'/lists/{list_.id}/')
+    # return redirect('view_list',list_.id)
+    return redirect(list_)
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -32,7 +33,7 @@ def view_list(request, list_id):
         item = Item(text = request.POST['item_text'], list = list_)
         item.full_clean()
         item.save()
-        return redirect(f'/lists/{list_.id}/')
+        return redirect(list_)
       except ValidationError:
         error = "You can't have an empty list item"
 
