8c92ff571f9b84278ae253aeec55d2a93a5e5636
Albert Fougy <albert@fougy.com>
2019-02-02 01:09:55 -0500

    fixed typo

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -27,7 +27,7 @@ class ItemValidationTest(FunctionalTest):
 
 		# He receives a similar warning on the list page
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('has-error').text,
+			self.browser.find_element_by_css_selector('.has-error').text,
 			"You can't have an empty list item"
 		))
 
