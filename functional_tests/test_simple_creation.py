461e79831f85014cf1ce163e0da7ec381c013fcd
Albert Fougy <albert@fougy.com>
2019-02-03 04:37:44 -0500

    rename all item input ids and names. still broken

--- functional_tests/test_simple_creation.py
+++ functional_tests/test_simple_creation.py
@@ -16,7 +16,7 @@ class NewVisitorTest(FunctionalTest):
 		self.assertIn('To-Do', header_text)
 
 		# He is invited to enter a to-do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
@@ -33,7 +33,7 @@ class NewVisitorTest(FunctionalTest):
 		# There is still a text box inviting him to add another item. He
 		# enters "Use peacock feathers to make a fly" (Albert is very
 		# methodical)
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
@@ -46,7 +46,7 @@ class NewVisitorTest(FunctionalTest):
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 			# Albert starts a new to-do list
 			self.browser.get(self.live_server_url)
-			inputbox = self.browser.find_element_by_id('id_new_item')
+			inputbox = self.get_item_input_box()
 			inputbox.send_keys('Buy peacock feathers')
 			inputbox.send_keys(Keys.ENTER)
 			self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -70,7 +70,7 @@ class NewVisitorTest(FunctionalTest):
 
 			# Francis starts a new list by entering a new item. He
 			# is less interesting than Albert...
-			inputbox = self.browser.find_element_by_id('id_new_item')
+			inputbox = self.get_item_input_box()
 			inputbox.send_keys('Buy milk')
 			inputbox.send_keys(Keys.ENTER)
 			self.wait_for_row_in_list_table('1: Buy milk')
--- lists/forms.py
+++ lists/forms.py
@@ -1,13 +1,6 @@
 from django import forms
 from lists.models import Item
 
-# class ItemForm(forms.Form):
-#     item_text = forms.CharField(
-#         widget = forms.fields.TextInput(attrs ={
-#             'placeholder': 'Enter a to-do item',
-#             'class': 'form-control input-lg',
-#         }),
-#     )
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 class ItemForm(forms.models.ModelForm):
 
