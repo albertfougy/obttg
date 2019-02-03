562ff37ad4278e1d36459c5d28161d1ee85d7d33
Albert Fougy <albert@fougy.com>
2019-02-03 14:20:43 -0500

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,30 +9,34 @@ class ItemValidationTest(FunctionalTest):
 		self.browser.get(self.live_server_url)
 		self.get_item_input_box().send_keys(Keys.ENTER)
 
-		# The home page refreshes, and there is an error message saying
-		# that list items cannot be blank
-		# Pass to a new helper method called "wait_for"
-		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text, 
-			"You can't have an empty list item"
+        # The browser intercepts the request, and does not load the
+        # list page
+		self.wait_for(lambda: self.browser.find_element_by_css_selector(
+			'#id_text:invalid'
 			))
 
-		# He tries again with some text for the item, which now works
+
+		# He starts typing some text for the new item and the error disappears 
 		self.get_item_input_box().send_keys('Buy milk')
-		self.get_item_input_box().send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy milk')
+		self.wait_for(lambda: self.browser.find_element_by_css_selector(
+			'#id_text:valid'
+		))
 
 		# Perversely, he now decides to submit a second blank list item
 		self.get_item_input_box().send_keys(Keys.ENTER)
 
-		# He receives a similar warning on the list page
-		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text, 
-			"You can't have an empty list item"
-			))
+
+		# Again, the browser will not comply
+		self.wait_for_row_in_list_table('1: Buy milk')
+		self.wait_for(lambda: self.browser.find_element_by_css_selector(
+			'#id_text:invalid'
+		))
 
 		# He corrects it by filling some text in
 		self.get_item_input_box().send_keys('Make tea')
+		self.wait_for(lambda: self.browser.find_element_by_css_selector(
+			'#id_text:valid'
+		))
 		self.get_item_input_box().send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
 		self.wait_for_row_in_list_table('2: Make tea')
--- lists/views.py
+++ lists/views.py
@@ -13,29 +13,21 @@ def home_page(request):
 #     return HttpResponse("OK")
 
 def new_list(request):
+  form = ItemForm(data=request.POST)
+  if form.is_valid():
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['text'], list = list_)
-    try:
-      item.full_clean()
-      item.save()
-    except ValidationError:
-      list_.delete()
-      error = "You can't have an empty list item"
-      return render(request, 'home.html', {"error" : error})
-    # return redirect('view_list',list_.id)
+    Item.objects.create(text=request.POST['text'], list=list_)
     return redirect(list_)
+  else:
+    return render(request, 'home.html', {"form": form})
+
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-
+    form = ItemForm()
     if request.method == 'POST':
-      try:
-        item = Item(text = request.POST['text'], list = list_)
-        item.full_clean()
-        item.save()
+      form = ItemForm(data=request.POST)
+      if form.is_valid():
+        Item.objects.create(text=request.POST['text'], list=list_)
         return redirect(list_)
-      except ValidationError:
-        error = "You can't have an empty list item"
-
-    return render(request, 'list.html', {'list': list_, 'error': error})
+    return render(request, 'list.html', {'list': list_, 'form': form})
