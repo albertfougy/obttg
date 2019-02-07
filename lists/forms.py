77209f4709769ad74f8c730f863d09f14ff5915b
Albert Fougy <albert@fougy.com>
2019-02-07 12:43:41 -0500

    Handled uniqueness validation

--- lists/forms.py
+++ lists/forms.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ValidationError
 from django import forms
 from lists.models import Item
 
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 class ItemForm(forms.models.ModelForm):
 
@@ -8,17 +10,28 @@ class ItemForm(forms.models.ModelForm):
         model = Item
         fields = ('text',)
         widgets = {
-            'text': forms.fields.TextInput(attrs ={
-                    'placeholder': 'Enter a to-do item',
-                    'class': 'form-control input-lg',
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
             }),
         }
         error_messages = {
-            'text': {'required': EMPTY_ITEM_ERROR }
+            'text': {'required': EMPTY_ITEM_ERROR}
         }
- 
+    
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
 
-    
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
+
