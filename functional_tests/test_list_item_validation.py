448feea3c828f0a052394ba7dca126c3b120673c
Albert Fougy <albert@fougy.com>
2019-02-13 02:24:59 -0500

    before dipping my toes tentatively into JS, performed at YAGNI

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,30 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+	def get_error_element(self):
+		return self.browser.find_element_by_css_selector('has-error')
+
+	def test_error_messages_are_cleaned_on_input(self):
+		# Albert starts a list and causes a validation error:
+		self.browser.get(self.live_server_url)
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Banter too thick')
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+
+		self.wait_for(lambda: self.assertTrue(
+			self.browser.get_error_element().is_displayed()
+		))
+
+		# He starts typing in the input box to clear the error
+		self.get_item_input_box().send_keys('a')
+
+		# He is pleased to see that the error message disappears
+		self.wait_for(lambda: self.assertFalse(
+			self.browser.browser.get_error_element().is_displayed()
+		))
+
 	def test_cannot_add_empty_list_items(self):
 		# Albert goes to the home page and accidentally tries to submit
 		# an empty list item. He hits Enter on the empty input box
@@ -58,6 +82,6 @@ class ItemValidationTest(FunctionalTest):
 
 		# He sees a helpful error message
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text,
+			self.browser.get_error_element().text,
 			"You've already got this in your list"
 		))
