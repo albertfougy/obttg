7f29ce3d6ba60f5cc06dcb06512d3b1e1ae850b2
Albert Fougy <albert@fougy.com>
2019-02-15 10:22:42 -0500

    "initialize is now part of window object

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,6 @@
 
-let initialize = () => {
+window.Superlists = {};
+window.Superlists.initialize = function(){
     $('input[name= "text"]').on('keypress', function(){
         $('.has-error').hide();
     });
