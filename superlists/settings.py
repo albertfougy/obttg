fd7f61b68bf0abd6156ebd4d139d380f30b2c8c3
Albert Fougy <albert@fougy.com>
2019-03-18 01:29:14 -0400

    django-behave in INSTALLED APPS for future BDD

--- superlists/settings.py
+++ superlists/settings.py
@@ -65,7 +65,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists'
+    'lists',
+    'behave_django',
 ]
 
 MIDDLEWARE = [
